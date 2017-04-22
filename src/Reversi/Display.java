d5c4eadd94d339df4a7a905239cde403fddb7cb8
Love4653 <Kaiya.park@gmail.com>
2017-04-21 18:36:45 -0700

    Started my game
    Made like one class

---
+++ src/Reversi/Display.java
@@ -0,0 +1,20 @@
+package Reversi;
+
+import javax.swing.JPanel;
+
+public class Display extends JPanel {
+	
+	private int width;
+	private int height;
+	
+	
+	public Display( int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/Reversi/GameManager.java
@@ -0,0 +1,6 @@
+package Reversi;
+
+public class GameManager {
+	
+
+}
